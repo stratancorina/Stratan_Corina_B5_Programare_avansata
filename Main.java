fe94eef489a798bbbc4072d88194e7cf84725510
stratancorina <53331115+stratancorina@users.noreply.github.com>
2022-03-03 13:30:26 +0200

    Homework2
    Homework2

--- Main.java
+++ Main.java
@@ -1,35 +1,70 @@
-package com.company;
+import java.util.Random;
+public class Main
+{
+    private static final Random rand = new Random();
+    public static void main(String[] args)
+    {
+        parseInput(args);
+        int n = Integer.parseInt(args[0]);
+        int p = Integer.parseInt(args[1]);
+        int lungAlfabet = args.length - 2;
+        char[] litere = new char[lungAlfabet];
+        for (int i = 0; i < lungAlfabet; i++)
+        {
+            litere[i] = args[i + 2].charAt(0);
+        }
 
-import static com.company.Type.LECTURE_HALL;
-import static com.company.Type.LABORATORY;
+        String[] cuvinte = generareCuvinte(n, p, litere, lungAlfabet);
 
-public class Main {
+        for (String cuvant : cuvinte)
+            System.out.println(cuvant);
+    }
 
-    public static void main(String[] args) {
-        //Events
-        Event C1 = new Event("C1", 100, 8, 10);
-        Event C2 = new Event("C2", 100, 10, 12);
-        Event L1 = new Event("L1", 30, 8, 10);
-        Event L2 = new Event("L2",10, 8, 10);
-        Event L3 = new Event("L3", 30, 10, 12);
+    public static void parseInput(String[] args) // Validarea argumentelor
+    {
+        if (args.length < 3)  // Verifica ca nr de arg sa fie mai mare decat 3
+        {
+            System.out.println("Nu sunt suficiente caractere'.");
+            System.exit(-1);
+        }
+        try    //Verifica ca primele 2 argumente sa fie integer uri
+        {
+            Integer.parseInt(args[0]); //verifca primul arg sa fie int
+            Integer.parseInt(args[1]); //verif pe al doilea
+        } catch (NumberFormatException e)
+        {
+            System.out.println("Variabile incorecte.");
+            System.exit(-1);
+        }
 
-        //Rooms
-        Room R1 = new Room("401", LABORATORY, 30 );
-        Room R2 = new Room("403", LABORATORY, 30 );
-        Room R3 = new Room("405", LABORATORY, 30 );
-        Room R4 = new Room("309", LECTURE_HALL, 100 );
-
-        System.out.println(C1);
-        System.out.println(C2);
-        System.out.println(L1);
-        System.out.println(R1);
-
-        if(L1.getNumOfParticipants() > 30) {
-            System.out.println( L1.getName() + "->" + R4.getName());
-        } else if (L1.getNumOfParticipants() <= 30){
-            System.out.println( L1.getName() + "->" + R2.getName());
+        for (int i = 2; i < args.length; i ++)
+        {
+            if(args[i].length() != 1)
+            {
+                System.out.println("Nu este o lungime valida.");
+                System.exit(-1);
+            }
+            if(!Character.isUpperCase(args[i].charAt(0)))
+            {
+                System.out.println("Nu sunt valide caractere.");
+                System.exit(-1);
+            }
         }
     }
 
-
+    public static String[] generareCuvinte(int n, int p, char[] litere, int lungAlfabet)
+    {
+        String[] cuvinte = new String[n];
+        for (int i = 0; i < n; i++) //n = 10 cuvinte
+        {
+            StringBuilder cuvant = new StringBuilder(); //strB
+            for (int j = 0; j < p; j++)
+            {
+                int z = rand.nextInt(lungAlfabet);
+                cuvant.append(litere[z]);
+            }
+            cuvinte[i] = cuvant.toString();
+        }
+        return cuvinte;
+    }
 }
