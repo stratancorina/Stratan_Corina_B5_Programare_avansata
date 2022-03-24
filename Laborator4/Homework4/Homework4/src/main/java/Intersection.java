0f727af9fd46e94cdf9ee8f3ef09ab0be56622ab
stratancorina <53331115+stratancorina@users.noreply.github.com>
2022-03-24 11:00:50 +0200

    Add files via upload

---
+++ Laborator4/Homework4/Homework4/src/main/java/Intersection.java
@@ -0,0 +1,39 @@
+import java.util.Objects;
+
+public class Intersection {
+
+    public String name;
+
+    public Intersection(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "Intersection{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Intersection that = (Intersection) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+}
